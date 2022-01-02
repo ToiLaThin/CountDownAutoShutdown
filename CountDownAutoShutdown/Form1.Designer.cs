d921a060d7c551bc7232be3648c2513cb4bb235a
Nguyen Duc Thinh <20110729@student.hcmute.edu.vn>
2022-01-03 04:21:23 +0700

    First commit

---
+++ CountDownAutoShutdown/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CountDownAutoShutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnShutDown = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.btnStopShut = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnShutDown
+            // 
+            this.btnShutDown.Location = new System.Drawing.Point(4, 169);
+            this.btnShutDown.Name = "btnShutDown";
+            this.btnShutDown.Size = new System.Drawing.Size(75, 23);
+            this.btnShutDown.TabIndex = 0;
+            this.btnShutDown.Text = "Shutdown";
+            this.btnShutDown.UseVisualStyleBackColor = true;
+            this.btnShutDown.Click += new System.EventHandler(this.btnShutDown_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(77, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(77, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(77, 61);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(77, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Min:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Second:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Remaining Time:";
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("Kristen ITC", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTime.Location = new System.Drawing.Point(27, 117);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(90, 40);
+            this.lbTime.TabIndex = 6;
+            this.lbTime.Text = "Does";
+            // 
+            // btnStopShut
+            // 
+            this.btnStopShut.Location = new System.Drawing.Point(93, 169);
+            this.btnStopShut.Name = "btnStopShut";
+            this.btnStopShut.Size = new System.Drawing.Size(75, 23);
+            this.btnStopShut.TabIndex = 7;
+            this.btnStopShut.Text = "Wait";
+            this.btnStopShut.UseVisualStyleBackColor = true;
+            this.btnStopShut.Click += new System.EventHandler(this.btnStopShut_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(170, 201);
+            this.Controls.Add(this.btnStopShut);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnShutDown);
+            this.Name = "Form1";
+            this.Text = "BiShutDown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShutDown;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Button btnStopShut;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CountDownAutoShutdown/Form1.cs
@@ -0,0 +1,78 @@
+﻿/*Idea comes from: https://www.youtube.com/watch?v=kwfc7QkCDZI */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CountDownAutoShutdown
+{
+    public partial class Form1 : Form
+    {
+        int secondsSelected;
+        int minutesSelected;
+        int totalSecRemaining;
+
+        TimeSpan timeDisplay;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i=0;i<60;i++)
+            {
+                this.comboBox1.Items.Add(i);
+                this.comboBox2.Items.Add(i);
+            }
+            this.comboBox1.SelectedItem = 59;
+            this.comboBox2.SelectedItem = 59;
+            this.lbTime.Text = "Time";
+            this.timer1.Interval = 1000;
+        }
+
+        private void btnShutDown_Click(object sender, EventArgs e)
+        {
+            this.secondsSelected = int.Parse(comboBox2.SelectedItem.ToString());
+            this.minutesSelected = int.Parse(comboBox1.SelectedItem.ToString());
+
+            this.totalSecRemaining = this.minutesSelected * 60 + this.secondsSelected;
+            btnShutDown.Enabled = false;
+            btnStopShut.Enabled = true;
+            timer1.Start();
+        }
+
+        private void btnStopShut_Click(object sender, EventArgs e)
+        {
+            btnStopShut.Enabled = false;
+            btnShutDown.Enabled = true;
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (totalSecRemaining > 0)
+            {
+                this.totalSecRemaining--;
+                timeDisplay = new TimeSpan(0, totalSecRemaining / 60, totalSecRemaining % 60);
+                lbTime.Text = this.timeDisplay.ToString();
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Time up!!!\n\aWe 're shutting down right now!!!");
+                System.Media.SoundPlayer audier = new System.Media.SoundPlayer();
+                audier.Play();
+                Process.Start("shutdown", "/s /t 0");
+            }
+
+        }
+    }
+}
